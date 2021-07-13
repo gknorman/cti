8c6fc8554ee235a0a1718f1948a95ea2838d42a4
Geoffrey Norman <geoffreyknorman@gmail.com>
2021-07-12 19:40:56 -0700

    Update firstMissingIntegerUnSortedOnProb.py
    Fixed missing parens and added more text to print statements.

--- 201/firstMissingIntegerUnSortedOnProb.py
+++ 201/firstMissingIntegerUnSortedOnProb.py
@@ -1,16 +1,16 @@
 # Given an unsorted integer array with max value n = length(integers), 
 # find the first missing positive integer in O(n) time with constant space.
 def first_missing_positive_integer_unsorted_On(integers):
-   n = len(integers)
-   result = partial(n) - sum(integers)
-   return n if result == 0 else result
+    n = len(integers)
+    result = partial(n) - sum(integers)
+    return n if result == 0 else result
    
 def partial(n):
     sums = (n * (n+1))/2
     return sums
-   n = len(integers)
-   result = partial(n) - sum(integers)
-   return n if result == 0 else result
+    n = len(integers)
+    result = partial(n) - sum(integers)
+    return n if result == 0 else result
    
 def partial(n):
     sums = (n * (n+1))/2
@@ -18,13 +18,13 @@ def partial(n):
     
 if __name__ == "__main__":
     print("Test 1: [0, 1,2] - should be: 3")
-    print(first_missing_positive_integer_unsorted_On([0, 1,2])
+    print("RESULT:", first_missing_positive_integer_unsorted_On([0, 1,2]))
     
     print("\nTest 2: [1,0,3,4] - should be: 2")
-    print(first_missing_positive_integer_unsorted_On([1,0,3,4])
+    print("RESULT:", first_missing_positive_integer_unsorted_On([1,0,3,4]))
     
     print("\nTest 3: [8, 9, 1, 2, 3, 4, 5, 7, 0] \n- should be: 6")
-    print(first_missing_positive_integer_unsorted_On([8, 9, 1, 2, 3, 4, 5, 7, 0])
+    print("RESULT:", first_missing_positive_integer_unsorted_On([8, 9, 1, 2, 3, 4, 5, 7, 0]))
     
     print("\nTest 4: [11, 14, 2, 3, 12, 4, 1, 5, 6, 7, 8, 9, 10, 0, 15] \n- should be: 13")
-    print(first_missing_positive_integer_unsorted_On([11, 14, 2, 3, 12, 4, 1, 5, 6, 7, 8, 9, 10, 0, 15])
+    print("RESULT:", first_missing_positive_integer_unsorted_On([11, 14, 2, 3, 12, 4, 1, 5, 6, 7, 8, 9, 10, 0, 15]))
